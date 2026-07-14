eef915a4a0573c17fe0aac3219fbe8f51c2e43af
Aditya Anand <adityaanandk1234@gmail.com>
2026-07-15 00:34:03 +0530

    ✅ Solved · Pascal's Triangle [Easy] · C++ · LeetCode (Array, Dynamic Programming)

--- DSA/LeetCode/Array/Pascal's Triangle/solution.cpp
+++ DSA/LeetCode/Array/Pascal's Triangle/solution.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
         
+        for (int i = 0; i < numRows; ++i) {
+            // Create a row of size (i + 1) filled with 1s
+            vector<int> row(i + 1, 1);
+            
+            // Fill in the inner elements using the row directly above
+            for (int j = 1; j < i; ++j) {
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+            
+            // Add the completed row to the triangle
+            triangle.push_back(row);
+        }
+        
+        return triangle;
     }
 };
